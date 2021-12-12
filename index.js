0c874c15809031d39dca48e005963d18714fa7a9
shu Lin <bbqlinshu@gmail.com>
2021-12-12 17:08:57 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  for(let i=0; i<word.length/2; i++){
+    const j = word.length - 1 - i
+    if(word[i] !== word[j]) return false
+  }
+  return true
 }
 
 /* 
   Add your pseudocode here
+  iterate from the beginning of the string to the middle of the string
+    compare the letter with the corresponding letter at the end of the tring
+      if any of the 2 pair of the letters don't match, return false
+  otherwise return true
 */
 
 /*
@@ -16,7 +25,8 @@ if (require.main === module) {
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("level"));
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
